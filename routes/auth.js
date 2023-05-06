f08f2ef14f5354fdd795d4c559814d35dd992c6e
Marius <marius.matulevicius1@gmail.com>
2023-05-06 16:46:16 +0300

    test

--- routes/auth.js
+++ routes/auth.js
@@ -3,5 +3,10 @@ const router = express.Router();
 
 router.post('/register');
 router.post('/login');
+router.get('test', (req, res) => {
+  res.json({
+    message: 'Hello World',
+  });
+});
 
 export default router;
